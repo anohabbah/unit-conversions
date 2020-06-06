368e06f818d574520fd334e267a8b4523cf16318
Abbah Anoh <anohabbah@gmail.com>
2020-06-06 12:23:16 +0200

    Temperature conversion

---
+++ src/Temperature.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace Abbah\UnitConversions;
+
+
+class Temperature
+{
+    private float $celsius;
+
+    public static function fromCelsius(float $celsius): self
+    {
+        return new static($celsius);
+    }
+
+    public function __construct(float $celsius)
+    {
+        $this->celsius = $celsius;
+    }
+
+    public function toFahrenheit(): float
+    {
+        return $this->celsius * 1.8 + 32;
+    }
+}
